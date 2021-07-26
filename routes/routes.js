c481aa0b918720ba153fc382336bfecb65013baf
LuanSSilva <52720974+luanpixel@users.noreply.github.com>
2021-07-26 09:42:58 -0300

    add api

--- routes/routes.js
+++ routes/routes.js
@@ -1,12 +1,12 @@
 module.exports = (app) => {
-    app.route("/title")
+    app.route("api/title")
         .get(app.api.list.get)
         .post(app.api.title.save)
         .post(app.api.gender.save)
         .post(app.api.description.save)
 
 
-    app.route("/title/:id")
+    app.route("api/title/:id")
         .get(app.api.getById.getById)
         .put(app.api.title.save)
         .delete(app.api.erase.erase)
