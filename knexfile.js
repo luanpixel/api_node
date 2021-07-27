a8d661da093256032f7f20d1ff8812edae661c92
LuanSSilva <52720974+luanpixel@users.noreply.github.com>
2021-07-26 21:28:40 -0300

    att

--- knexfile.js
+++ knexfile.js
@@ -1,16 +1,9 @@
 require('dotenv').config()
 module.exports = {
     client: "pg",
-    connection: {
-        host: process.env.DATABASE_URL,
-        database: process.env.DB_DATABASE,
-        user: process.env.DB_USER,
-        password: process.env.DB_PASS
-    },
-    poll: {
-        min: 2,
-        max: 10
-    },
+    connection:
+        process.env.DATABASE_URL,
+
     migrations: {
         tableName: "Knex_migrations"
     }
