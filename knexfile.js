5f68e17b407af3b72d8e57537ff882a0b7f70c06
LuanSSilva <52720974+luanpixel@users.noreply.github.com>
2021-07-26 21:08:04 -0300

    att

--- knexfile.js
+++ knexfile.js
@@ -1,8 +1,9 @@
 require('dotenv').config()
 module.exports = {
-    client: "postgres",
+    client: "pg",
     connection: {
         host: process.env.DB_HOST,
+        port: process.env.DB_PORT,
         database: process.env.DB_DATABASE,
         user: process.env.DB_USER,
         password: process.env.DB_PASS
